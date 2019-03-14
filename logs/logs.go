6a61e5f24a514eea55a3a58625f517f54de73221
biaobiaohehe <biaohe1231@gmial.com>
2019-03-15 00:29:57 +0800

    add log struct

--- logs/logs.go
+++ logs/logs.go
@@ -15,3 +15,40 @@ import (
 const (
 	Level = iota
 )
+
+var (
+	sequenceNo uint64
+	instance   *Logger
+)
+
+var logLevel = levelInfo
+
+type Logger struct {
+	mu     sync.Mutex
+	output io.Writer
+}
+
+type LogRecord struct {
+	ID string
+
+	Level string
+
+	Message string
+
+	Filename string
+
+	LineNo int
+}
+
+var Log = GetLogger(os.Stdout)
+
+var (
+	LogRecordTemplate      *template.Temlate
+	debugLogRecordTemplate *template.Temlate
+)
+
+func GetLogger(w io.Writer) *BeeLogger {
+	once.Do(func() {
+
+	})
+}
